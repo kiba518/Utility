6cc7de61c165e0153fc94dc1c3b5fd5ceebbca90
kiba518 <liujin_hzzh@126.com>
2019-07-11 11:17:03 +0800

    ImageHelper
    ImageHelper

--- Utility/Tool/ImageHelper.cs
+++ Utility/Tool/ImageHelper.cs
@@ -6,11 +6,63 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Imaging;
 
 namespace Utility 
 {
     public static class ImageHelper
     {
+        public static byte[] GetImage(string path)
+        {
+            byte[] ret = null;
+            var mem = new System.IO.MemoryStream(System.IO.File.ReadAllBytes(path));
+            ret = mem.ToArray();
+            mem.Close();
+            return ret;
+        }
+        public static BitmapImage BytesToBitmapImage(byte[] byteArray)
+        {
+            BitmapImage bmp = null;
+
+            try
+            {
+                bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = new MemoryStream(byteArray);
+                bmp.EndInit();
+            }
+            catch
+            {
+                bmp = null;
+            }
+            return bmp;
+        }
+        public static byte[] BitmapImageToBytes(BitmapImage bmp)
+        {
+            byte[] byteArray = null;
+
+            try
+            {
+                Stream sMarket = bmp.StreamSource;
+
+                if (sMarket != null && sMarket.Length > 0)
+                {
+                    //很重要，因为Position经常位于Stream的末尾，导致下面读取到的长度为0。 
+                    sMarket.Position = 0;
+
+                    using (BinaryReader br = new BinaryReader(sMarket))
+                    {
+                        byteArray = br.ReadBytes((int)sMarket.Length);
+                    }
+                }
+            }
+            catch
+            {
+                //other exception handling 
+            }
+
+            return byteArray;
+        }
         /// <summary>
         /// Convert Image to Byte[]
         /// </summary>
