835627b74b85650db07fcdfa4d6a77c51f1b7008
kiba518 <liujin_hzzh@126.com>
2019-11-08 10:37:23 +0800

    增加Base64转码帮助类

---
+++ Utility/Tool/Base64Helper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio;
+using NAudio.CoreAudioApi;
+
+namespace Utility
+{
+    public class Base64Helper
+    {
+        /// <summary>
+        /// 编码
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="code_type"></param> 
+        /// <returns></returns>
+        public static string EncodeBase64(byte[] code, string code_type = "utf-8")
+        {
+            string encode = "";
+            byte[] bytes = code;
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = "";
+            }
+            return encode;
+        }
+        /// <summary>
+        /// 编码
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="code_type"></param> 
+        /// <returns></returns>
+        public static string EncodeBase64(string code, string code_type = "utf-8")
+        {
+            string encode = "";
+            byte[] bytes = Encoding.GetEncoding(code_type).GetBytes(code);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = code;
+            }
+            return encode;
+        }
+        /// <summary>
+        /// 解码
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="code_type"></param> 
+        /// <returns></returns>
+        public static string DecodeBase64(string code, string code_type = "utf-8")
+        {
+            string decode = "";
+            byte[] bytes = Convert.FromBase64String(code);
+            try
+            {
+                decode = Encoding.GetEncoding(code_type).GetString(bytes);
+            }
+            catch
+            {
+                decode = code;
+            }
+            return decode;
+        }
+        
+    } 
+}
--- Utility/Tool/JsonHelper.cs
+++ Utility/Tool/JsonHelper.cs
@@ -56,7 +56,7 @@ namespace Utility
         /// <param name="json">json字符串(eg.{"ID":"112","Name":"石子儿"})</param>
         /// <returns>对象实体</returns>
         public static T DeserializeJsonToObject<T>(string json) where T : class
-        {
+        { 
             JsonSerializer serializer = new JsonSerializer();
             StringReader sr = new StringReader(json);
             object o = serializer.Deserialize(new JsonTextReader(sr), typeof(T));
