2bab5fb1624576290fb4b5e9e590cba9f1befb63
liujin <liujin_hzzh@126.com>
2020-06-11 10:32:17 +0800

    Rsa加密和电脑信息帮助

---
+++ Utility/Cert/DataCertificateHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Utility;
+namespace Utility
+{
+    public  class DataCertificateHelper
+    {
+        //加密
+        public const string publicKey = "<RSAKeyValue><Modulus>zz7IQIJC6B56EdskOinWpRXY9mAfjcoNw3VRk1Q0FpYt+uOncqman6/WjG2LusSxAz+WmQKb0HoqLLTN/YluJLRNJMCkcB9bRATrFBjWkA3QMyoqmr+QPERzCzUVoceGWys1H0odpi/v+cL6GdgbhGcUxWVcUtDXcmDMv/b6UpE=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+        //解密
+        public const string privateKey = "<RSAKeyValue><Modulus>zz7IQIJC6B56EdskOinWpRXY9mAfjcoNw3VRk1Q0FpYt+uOncqman6/WjG2LusSxAz+WmQKb0HoqLLTN/YluJLRNJMCkcB9bRATrFBjWkA3QMyoqmr+QPERzCzUVoceGWys1H0odpi/v+cL6GdgbhGcUxWVcUtDXcmDMv/b6UpE=</Modulus><Exponent>AQAB</Exponent><P>6N8DMG8JMaQlPfiHOCeMWhuRMw4PGj1SLcLglTMYSFwx6ZYHLHb/cCg56ou4AynPEUnaTRXL8yFBpsiprmZ5mw==</P><Q>49Q0e86O4uSiqF8GfO6Ke3IyHXGrEhV4WHgfwqUz4t2q2cmpOtXQrOmhvqoipI71eDqas+aJpmACWxpPrfztQw==</Q><DP>yQxJD5a9hEsoEBGyhGu4g88LE94OceQBQBrglE9xpn9aZEWv2da/ABDqt7F649hDurRdMXIhC75plNnnjPdSAw==</DP><DQ>UY4RC5CxjX8SyvrZM7egUvhaADhEaMDOx7yYgfDpVfjLxBJwReNsQ7mOcNYueIHEVTmFT4jjFW+g6EPO6hV0SQ==</DQ><InverseQ>HKXnLS+GNcXXcP8sFPpMlBq2q5PAIyTkPyhtmnIw4ID1F4EIY6OxTkdrlZsWS/rbzCGR7qGgc1k/t/rN3BWn3g==</InverseQ><D>wwhXVfYAVwdPY3xypFX5TNS5oGqnZz0uJLJEeby1ZEgir0gMriiUfhDxfKge5j3yQ5dU91vwHIrLI9VnQWFTB4AjaGxZuJi+qtXviXpYiPt78lzKpZuLKH6VqQu5i9aSWicHQdosJDoPVA51k+3KELvG8xDzEI5CCVOC90dGHj0=</D></RSAKeyValue>";
+
+        public static  void CopyPfxAndGetInfo()
+        { 
+            string keyName = "VanPeng.Desktop.Licence";
+            var ret = DataCertificate.CreateCertWithPrivateKey(keyName, @"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\makecert.exe");
+            if(ret)
+            {
+                DataCertificate.ExportToPfxFile(keyName, "VanPeng.pfx", "123456", true);
+                X509Certificate2 x509 = DataCertificate.GetCertificateFromPfxFile("VanPeng.pfx", "123456");
+                string publickey = x509.PublicKey.Key.ToXmlString(false);
+                string privatekey = x509.PrivateKey.ToXmlString(true);
+
+                string myname = "my name is VanPeng.Desktop.Licence!";
+                string enStr = RSAEncrypt(publickey, myname); 
+                string deStr = RSADecrypt(privatekey, enStr); 
+            }
+        }
+        public static void TestRsa()
+        {
+            string myname = "my name is VanPeng.Desktop.Licence!";
+            string enStr = RSAEncrypt(publicKey, myname);
+            string deStr = RSADecrypt(privateKey, enStr);
+        }
+       
+   /// <summary> 
+        /// RSA解密 
+        /// </summary> 
+        /// <param name="xmlPrivateKey"></param> 
+        /// <param name="m_strDecryptString"></param> 
+        /// <returns></returns> 
+        public static string RSADecrypt(string xmlPrivateKey, string m_strDecryptString)
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(xmlPrivateKey);
+            byte[] rgb = Convert.FromBase64String(m_strDecryptString);
+            byte[] bytes = provider.Decrypt(rgb, false);
+            return new UnicodeEncoding().GetString(bytes);
+        }
+        /// <summary> 
+        /// RSA加密 
+        /// </summary> 
+        /// <param name="xmlPublicKey"></param> 
+        /// <param name="m_strEncryptString"></param> 
+        /// <returns></returns> 
+        public static string RSAEncrypt(string xmlPublicKey, string m_strEncryptString)
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(xmlPublicKey);
+            byte[] bytes = new UnicodeEncoding().GetBytes(m_strEncryptString);
+            return Convert.ToBase64String(provider.Encrypt(bytes, false));
+        }
+       
+      
+    }
+}
---
+++ Utility/Computer/ComputerHelper.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class ComputerHelper
+    {
+        //获取硬盘序列号
+        public static string GetHardDiskSerialNumber()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia");
+                string sHardDiskSerialNumber = "";
+                foreach (ManagementObject mo in searcher.Get())
+                {
+                    sHardDiskSerialNumber = mo["SerialNumber"].ToString().Trim();
+                    break;
+                }
+                return sHardDiskSerialNumber;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// 获取CPU序列号
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCPUSerialNumber()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Processor");
+                string sCPUSerialNumber = "";
+                foreach (ManagementObject mo in searcher.Get())
+                {
+                    sCPUSerialNumber = mo["ProcessorId"].ToString().Trim();
+                }
+                return sCPUSerialNumber;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// 获取主板序列号
+        /// </summary>
+        /// <returns></returns>
+        public static string GetBIOSSerialNumber()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_BIOS");
+                string sBIOSSerialNumber = "";
+                foreach (ManagementObject mo in searcher.Get())
+                {
+                    sBIOSSerialNumber = mo["SerialNumber"].ToString().Trim();
+                }
+                return sBIOSSerialNumber;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        //只能获取同网段的远程主机MAC地址. 因为在标准网络协议下，ARP包是不能跨网段传输的，故想通过ARP协议是无法查询跨网段设备MAC地址的。
+        [DllImport("Iphlpapi.dll")]
+        private static extern int SendARP(Int32 dest, Int32 host, ref Int64 mac, ref Int32 length);
+        [DllImport("Ws2_32.dll")]
+        private static extern Int32 inet_addr(string ip);
+
+        /// <summary>
+        /// 获取ip对应的MAC地址
+        /// </summary>
+        public static string GetMacAddress(string ip)
+        {
+            Int32 ldest = inet_addr(ip);            //目的ip 
+            Int32 lhost = inet_addr("127.0.0.1");   //本地ip 
+
+
+            try
+            {
+                Int64 macinfo = new Int64();
+                Int32 len = 6;
+                int res = SendARP(ldest, 0, ref macinfo, ref len);  //使用系统API接口发送ARP请求，解析ip对应的Mac地址
+                return Convert.ToString(macinfo, 16);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error:{0}", err.Message);
+            }
+            return "获取Mac地址失败";
+        }
+
+        /// <summary>
+        /// 获取外网ip地址
+        /// </summary>
+        public static string[] GetExtenalIpAddress()
+        {
+            string[] IP = new string[] { "未获取到外网ip", "" };
+
+
+            string address = "http://1111.ip138.com/ic.asp";
+            string str = GetWebStr(address);
+
+
+            try
+            {
+                //提取外网ip数据 [218.104.71.178]
+                int i1 = str.IndexOf("[") + 1, i2 = str.IndexOf("]");
+                IP[0] = str.Substring(i1, i2 - i1);
+
+
+                //提取网址说明信息 "来自：安徽省合肥市 联通"
+                i1 = i2 + 2; i2 = str.IndexOf("<", i1);
+                IP[1] = str.Substring(i1, i2 - i1);
+            }
+            catch (Exception) { }
+
+
+            return IP;
+        }
+        /// <summary>
+        /// 获取网址address的返回的文本串数据
+        /// </summary>
+        public static string GetWebStr(string address)
+        {
+            string str = "";
+            try
+            {
+                //从网址中获取本机ip数据
+                System.Net.WebClient client = new System.Net.WebClient();
+                client.Encoding = System.Text.Encoding.Default;
+                str = client.DownloadString(address);
+                client.Dispose();
+            }
+            catch (Exception) { }
+
+
+            return str;
+        }
+        /// <summary>
+        /// 获取本地ip地址，多个ip
+        /// </summary>
+        public static String[] GetLocalIpAddress()
+        {
+            string hostName = Dns.GetHostName();                    //获取主机名称
+            IPAddress[] addresses = Dns.GetHostAddresses(hostName); //解析主机IP地址
+
+
+            string[] IP = new string[addresses.Length];             //转换为字符串形式
+            for (int i = 0; i < addresses.Length; i++) IP[i] = addresses[i].ToString();
+
+
+            return IP;
+        }
+        /// <summary>
+        /// 操作系统的登录用户名
+        /// </summary>
+        public static string GetUserName()
+        {
+            return Environment.UserName;
+        }
+        /// <summary>
+        /// 获取计算机名
+        /// </summary>
+        public static string GetComputerName()
+        {
+            return Environment.MachineName;
+        }
+        /// <summary>
+        /// 操作系统类型
+        /// </summary>
+        public static string GetSystemType()
+        {
+            string st = "";
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                st = mo["SystemType"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// 物理内存
+        /// </summary>
+        public static string GetPhysicalMemory()
+        {
+            string st = "";
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                st = mo["TotalPhysicalMemory"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// 显卡PNPDeviceID
+        /// </summary>
+        public static string GetVideoPNPID()
+        {
+            string st = "";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_VideoController");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                st = mo["PNPDeviceID"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// 声卡PNPDeviceID
+        /// </summary>
+        public static string GetSoundPNPID()
+        {
+            string st = "";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_SoundDevice");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                st = mo["PNPDeviceID"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// CPU版本信息
+        /// </summary>
+        public static string GetCPUVersion()
+        {
+            string st = "";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_Processor");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                st = mo["Version"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// CPU名称信息
+        /// </summary>
+        public static string GetCPUName()
+        {
+            string st = "";
+            ManagementObjectSearcher driveID = new ManagementObjectSearcher("Select * from Win32_Processor");
+            foreach (ManagementObject mo in driveID.Get())
+            {
+                st = mo["Name"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// CPU制造厂商
+        /// </summary>
+        public static string GetCPUManufacturer()
+        {
+            string st = "";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_Processor");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                st = mo["Manufacturer"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// 主板制造厂商
+        /// </summary>
+        public static string GetBoardManufacturer()
+        {
+            SelectQuery query = new SelectQuery("Select * from Win32_BaseBoard");
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(query);
+            ManagementObjectCollection.ManagementObjectEnumerator data = mos.Get().GetEnumerator();
+            data.MoveNext();
+            ManagementBaseObject board = data.Current;
+            return board.GetPropertyValue("Manufacturer").ToString();
+        }
+
+
+        /// <summary>
+        /// 主板编号
+        /// </summary>
+        public static string GetBoardID()
+        {
+            string st = "";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_BaseBoard");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                st = mo["SerialNumber"].ToString();
+            }
+            return st;
+        }
+
+
+        /// <summary>
+        /// 主板型号
+        /// </summary>
+        public static string GetBoardType()
+        {
+            string st = "";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_BaseBoard");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                st = mo["Product"].ToString();
+            }
+            return st;
+        }
+
+
+    }
+}
